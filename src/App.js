02c34ba554d252476befc3b04809c7b1a7d45879
Luis Alejandro Flores Madrigal <luis-flores@ibm.com>
2024-09-19 09:33:56 -0600

    Revert "Fixing routes"
    This reverts commit 777507d18d1394c2662edbcca654286a6c570e24.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  HashRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom"; // HashRouter added
+import { Routes, Route, Navigate } from "react-router-dom"; // Added Navigate
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
@@ -28,23 +23,18 @@ const App = () => {
       <CssBaseline />
       <Header />
       <div className="app-body" style={{ paddingTop: "4rem" }}>
-        <Router>
-          {" "}
-          {/* Switch to HashRouter */}
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Navigate to="/home" replace />} />{" "}
-              {/* Redirect / to /home */}
-              <Route path="/home" element={<Home />} />{" "}
-              {/* Add the home route */}
-              <Route path="/boda-civil" element={<BodaCivil />} />
-              <Route path="/te-amo-3-millones" element={<TeAmo3Millones />} />
-              <Route path="/flores-amarillas" element={<FloresAmarillas />} />
-              <Route path="/building" element={<Building />} />
-              <Route path="*" element={<NotFound />} />
-            </Route>
-          </Routes>
-        </Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Navigate to="/home" replace />} />{" "}
+            {/* Redirect / to /home */}
+            <Route path="/home" element={<Home />} /> {/* Add the home route */}
+            <Route path="/boda-civil" element={<BodaCivil />} />
+            <Route path="/te-amo-3-millones" element={<TeAmo3Millones />} />
+            <Route path="/flores-amarillas" element={<FloresAmarillas />} />
+            <Route path="/building" element={<Building />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
       </div>
     </ThemeProvider>
   );
