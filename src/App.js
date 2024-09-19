5bad2556d190261d436b619c62696cde8e8479c4
Luis Alejandro Flores Madrigal <luis-flores@ibm.com>
2024-09-19 04:17:35 -0600

    Update routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom"; // Added Navigate
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
@@ -25,7 +25,9 @@ const App = () => {
       <div className="app-body" style={{ paddingTop: "4rem" }}>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
+            <Route index element={<Navigate to="/home" replace />} />{" "}
+            {/* Redirect / to /home */}
+            <Route path="/home" element={<Home />} /> {/* Add the home route */}
             <Route path="/boda-civil" element={<BodaCivil />} />
             <Route path="/te-amo-3-millones" element={<TeAmo3Millones />} />
             <Route path="/flores-amarillas" element={<FloresAmarillas />} />
