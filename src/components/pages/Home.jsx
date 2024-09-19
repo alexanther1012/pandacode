466b5b6cb7d9a3ad63a5efbc279bc7cc7b7243c8
Luis Alejandro Flores Madrigal <luis-flores@ibm.com>
2024-09-19 09:33:46 -0600

    Import Flores Amarillas

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,43 +1,48 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Button from "@mui/material/Button";
+import FloresAmarillas from "./FloresAmarillas";
 
 const Home = () => {
+  const [flowers, setFlowers] = useState(false);
   return (
     <Fragment>
-      <div className="centered-container">
-        <div className="greetings">
-          <span>H</span>
-          <span>O</span>
-          <span>L</span>
-          <span>A</span>
+      {!flowers && (
+        <div className="centered-container">
+          <div className="greetings">
+            <span>H</span>
+            <span>O</span>
+            <span>L</span>
+            <span>A</span>
+          </div>
+          <div className="greetings">
+            <span>M</span>
+            <span>I</span>
+          </div>
+          <div className="greetings">
+            <span>A</span>
+            <span>M</span>
+            <span>O</span>
+            <span>R</span>
+            <span>O</span>
+            <span>S</span>
+            <span>I</span>
+            <span>T</span>
+            <span>A</span>
+          </div>
+          <div className="description">
+            <span>Tengo un regalo muy especial para ti!!</span>
+          </div>
+          <Button
+            variant="contained"
+            color="primary"
+            className="button"
+            onClick={() => setFlowers(!flowers)}
+          >
+            Haz click aqui
+          </Button>
         </div>
-        <div className="greetings">
-          <span>M</span>
-          <span>I</span>
-        </div>
-        <div className="greetings">
-          <span>A</span>
-          <span>M</span>
-          <span>O</span>
-          <span>R</span>
-          <span>O</span>
-          <span>S</span>
-          <span>I</span>
-          <span>T</span>
-          <span>A</span>
-        </div>
-        <div className="description">
-          <span>Tengo un regalo muy especial para ti!!</span>
-        </div>
-        <Button
-          href="/flores-amarillas"
-          variant="contained"
-          color="primary"
-          className="button"
-        >
-          Haz click aqui
-        </Button>
-      </div>
+      )}
+      {flowers && <FloresAmarillas />}
     </Fragment>
   );
 };
