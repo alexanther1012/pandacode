777507d18d1394c2662edbcca654286a6c570e24
Luis Alejandro Flores Madrigal <luis-flores@ibm.com>
2024-09-19 04:49:06 -0600

    Fixing routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom"; // Added Navigate
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom"; // HashRouter added
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
@@ -23,18 +28,23 @@ const App = () => {
       <CssBaseline />
       <Header />
       <div className="app-body" style={{ paddingTop: "4rem" }}>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Navigate to="/home" replace />} />{" "}
-            {/* Redirect / to /home */}
-            <Route path="/home" element={<Home />} /> {/* Add the home route */}
-            <Route path="/boda-civil" element={<BodaCivil />} />
-            <Route path="/te-amo-3-millones" element={<TeAmo3Millones />} />
-            <Route path="/flores-amarillas" element={<FloresAmarillas />} />
-            <Route path="/building" element={<Building />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
+        <Router>
+          {" "}
+          {/* Switch to HashRouter */}
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Navigate to="/home" replace />} />{" "}
+              {/* Redirect / to /home */}
+              <Route path="/home" element={<Home />} />{" "}
+              {/* Add the home route */}
+              <Route path="/boda-civil" element={<BodaCivil />} />
+              <Route path="/te-amo-3-millones" element={<TeAmo3Millones />} />
+              <Route path="/flores-amarillas" element={<FloresAmarillas />} />
+              <Route path="/building" element={<Building />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </Router>
       </div>
     </ThemeProvider>
   );
