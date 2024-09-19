4360481d2b4f07cea50ed042f351e7640872164b
Luis Alejandro Flores Madrigal <luis-flores@ibm.com>
2024-09-19 10:19:15 -0600

    Fixing routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // Use HashRouter instead of BrowserRouter
 import App from "./App";
 import "./index.css";
 
@@ -8,9 +8,7 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
